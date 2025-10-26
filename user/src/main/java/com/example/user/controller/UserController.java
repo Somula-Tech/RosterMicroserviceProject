eda181ab07ad0d51e65acc3eb6389d60539cb465
Admin <somularamalingeswarreddy2@gmail.com>
2025-10-26 16:03:24 +0530

    Modified in controller class

--- user/src/main/java/com/example/user/controller/UserController.java
+++ user/src/main/java/com/example/user/controller/UserController.java
@@ -11,6 +11,6 @@ public class UserController {
 
     @GetMapping(value = "/users")
     public ResponseEntity<String> userProfile(){
-        return ResponseEntity.ok("Successfully fetched the User");
+        return ResponseEntity.ok("Successfully fetched the Users");
     }
 }
